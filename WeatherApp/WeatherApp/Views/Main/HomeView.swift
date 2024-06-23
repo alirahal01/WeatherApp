56be70541eba8f6099f978fc79788324ade9a0e9
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-06-23 21:50:32 +0300

    Add homeview and colors extension

---
+++ WeatherApp/WeatherApp/Views/Main/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  WeatherApp
+//
+//  Created by ali rahal on 23/06/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
