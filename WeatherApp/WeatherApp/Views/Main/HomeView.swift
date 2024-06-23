664ed0a950da02c57570ccc5c0586854a01e6251
ali.rahal01@gmail.com <ali.rahal01@gmail.com>
2024-06-23 23:02:32 +0300

    Implement homeview

--- WeatherApp/WeatherApp/Views/Main/HomeView.swift
+++ WeatherApp/WeatherApp/Views/Main/HomeView.swift
@@ -9,10 +9,38 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //  MARK: Backround Color
+            Color.background
+                .ignoresSafeArea()
+            
+            // MARK: Background Image
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            // MARK: House Image
+            Image("House")
+                .frame(maxHeight: .infinity, alignment: .top)
+                .padding(.top, 257)
+            
+            // MARK:
+            VStack {
+                Text("Montreal")
+                    .font(.largeTitle)
+                VStack {
+                    Text("19°" + "\n" + "Mostly Clear")
+                    Text("H:24°   L:18°")
+                        .font(.title3.weight(.semibold))
+                }
+                Spacer()
+            }
+            .padding(.top, 51)
+        }
     }
 }
 
 #Preview {
     HomeView()
+        .preferredColorScheme(.dark)
 }
